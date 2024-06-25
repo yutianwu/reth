14ebc2c383c60c4095ea6cc8aab21113f3ca8796
yutianwu <wzxingbupt@gmail.com>
2024-06-25 16:39:12 +0800

    feat: add bootnodes for opbnb (#41)

--- crates/primitives/src/lib.rs
+++ crates/primitives/src/lib.rs
@@ -131,7 +131,10 @@ mod optimism {
         BASE_MAINNET, BASE_SEPOLIA, OP_MAINNET, OP_SEPOLIA,
     };
     #[cfg(feature = "opbnb")]
-    pub use reth_chainspec::{OPBNB_MAINNET, OPBNB_TESTNET};
+    pub use reth_chainspec::{
+        net::{opbnb_mainnet_nodes, opbnb_testnet_nodes},
+        OPBNB_MAINNET, OPBNB_TESTNET,
+    };
 }
 
 #[cfg(feature = "optimism")]
